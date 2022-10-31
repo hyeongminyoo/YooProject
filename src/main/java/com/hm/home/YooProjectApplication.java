ad0e9607121c91eee387683d351ce69344b46f7c
hyeongminyoo <dbgudals2004@gmail.com>
2022-10-31 14:34:14 +0900

    init

---
+++ src/main/java/com/hm/home/YooProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hm.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YooProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YooProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hm/home/YooProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hm.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YooProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
